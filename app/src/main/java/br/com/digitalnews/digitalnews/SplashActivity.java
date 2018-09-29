8894cdb8bacf46c3b25ee6210a9b28cc834f13a9
samirassaf <samir_assaf_1@hotmail.com>
2018-09-29 09:34:57 -0300

    tela de splash

---
+++ app/src/main/java/br/com/digitalnews/digitalnews/SplashActivity.java
@@ -0,0 +1,30 @@
+package br.com.digitalnews.digitalnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = findViewById(R.id.imageview_splash);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+
+
+
+}
