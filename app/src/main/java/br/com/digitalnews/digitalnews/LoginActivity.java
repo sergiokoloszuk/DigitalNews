4bcf53000de3c61b8576270d3c0e338d7fe3ab3e
Rosana <rosanaflag@gmail.com>
2018-09-28 22:05:42 -0300

    tela de registro Rosana

--- app/src/main/java/br/com/digitalnews/digitalnews/LoginActivity.java
+++ app/src/main/java/br/com/digitalnews/digitalnews/LoginActivity.java
@@ -1,13 +1,32 @@
 package br.com.digitalnews.digitalnews;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    register = findViewById(R.id.register);
+
+    register.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+
+        }
+    });
+
+
     }
 }
