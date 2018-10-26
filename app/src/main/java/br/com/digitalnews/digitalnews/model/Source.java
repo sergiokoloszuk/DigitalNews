430457c3e3a8168640a33cbc3f4e284bb712f39f
leehendryp <leehendryp@gmail.com>
2018-10-25 21:45:55 -0300

    Atualização com a API

---
+++ app/src/main/java/br/com/digitalnews/digitalnews/model/Source.java
@@ -0,0 +1,29 @@
+
+package br.com.digitalnews.digitalnews.model;
+
+import com.google.gson.annotations.Expose;
+
+public class Source {
+
+    @Expose
+    private String id;
+    @Expose
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
