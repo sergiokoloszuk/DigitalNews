d27d87e4f4c2eaede3da1f0f1bcc4e09d1ffe578
sergiokoloszuk <sergiokoloszuk.sk@gmail.com>
2018-12-04 17:32:42 -0200

    sergio

--- app/src/main/java/br/com/digitalnews/digitalnews/LoginActivity.java
+++ app/src/main/java/br/com/digitalnews/digitalnews/LoginActivity.java
@@ -1,25 +1,36 @@
 package br.com.digitalnews.digitalnews;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class LoginActivity extends AppCompatActivity {
 
     private TextView register;
-    private Button btnLogin;
+    private LoginButton btnLogin;
+    
 
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         register = findViewById(R.id.register);
-        btnLogin = findViewById(R.id.btn_login);
+        btnLogin = (LoginButton)findViewById(R.id.btn_login);
+
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,11 +40,46 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+
+        btnLogin.setReadPermissions("email");
+        
+        // Callback registration
+        btnLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
+
+
+        
+        
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        PreferenceManager.OnActivityResultListener callbackManager;
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
