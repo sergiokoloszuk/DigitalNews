29c89cbe5d73ca04cd97386fa83546241ad90ced
sergiokoloszuk <sergiokoloszuk.sk@gmail.com>
2018-12-08 15:53:22 -0200

    Splash gerando chave hash

--- app/src/main/java/br/com/digitalnews/digitalnews/SplashActivity.java
+++ app/src/main/java/br/com/digitalnews/digitalnews/SplashActivity.java
@@ -1,12 +1,19 @@
 package br.com.digitalnews.digitalnews;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -17,7 +24,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        getHash();
         imageView = findViewById(R.id.imageview_splash);
        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +44,22 @@ public class SplashActivity extends AppCompatActivity {
     private void skip() {
         startActivity(new Intent(SplashActivity.this, LoginActivity.class));
         finish();
+    }
+
+    private void getHash (){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "br.com.digitalnews.digitalnews",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash facebook:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
